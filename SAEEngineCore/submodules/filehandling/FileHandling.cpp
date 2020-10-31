d2c9c15d696325a27d5c2c772a5af3d369a3b353
JonathanCline <59320680+JonathanCline@users.noreply.github.com>
2020-10-30 21:44:02 -0500

    Did a bit of code review

--- SAEEngineCore/submodules/filehandling/FileHandling.cpp
+++ SAEEngineCore/submodules/filehandling/FileHandling.cpp
@@ -13,10 +13,10 @@ namespace sae::engine::core
 		try
 		{
 
-			std::ifstream file;
+			std::ifstream file;		// good job using an ifstream as thats all that is needed
 			file.open(filename);
 
-			std::string data;
+			std::string data;		// consider using a std::vector<char> for the data storage as strings are not ment to store information
 			file >> data;
 
 			/*TODO: remove after being done with debugging
@@ -24,7 +24,7 @@ namespace sae::engine::core
 			*/
 			lout << "data: " << data << "\n";
 
-			return data;
+			return data;			// Consider creating a type for storing a loaded file's data
 
 		}
 		// TODO: not sure if this catches file reading errors please tell me if it doesn't
@@ -32,10 +32,14 @@ namespace sae::engine::core
 		{
 
 			lout << "Error opening " << filename;
-			return "ERROR";
+			return "ERROR";				// Id recommend against returning an error as a string as string comparisons are slow
+										// Consider returning a std::optional<T> where T is your data storage type
 
 		};
 		
+		// Go look at the documentation for fstreams and see how error checking and handling is preformed.
+		//		https://en.cppreference.com/w/cpp/io/basic_fstream
+
 	}
 
 }
